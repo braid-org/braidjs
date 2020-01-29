250fccd1dba73e1dfba941882c9512828b7d5e60
Michael Toomim <toomim@gmail.com>
2020-01-28 16:11:50 -0800

    Add (unfinished) http client and server

--- http-client.js
+++ http-client.js
@@ -1,1 +1,221 @@
-// Example braid-peer as a web browser client will go here
+// Example braid-peer as a web browser client
+
+module.exports = require['http-client'] = function add_http_client(node) {
+    var u = require('./utilities.js')
+
+    var prefix = '/*', url = 'http://localhost:3007/', client_creds = null
+    var preprefix = prefix.slice(0,-1)
+
+    var pipe = require('./pipe.js')({node, id: 'client-pipe', send, connect})
+    node.bind(prefix, pipe)
+
+    var is_absolute = /^https?:\/\//
+    var has_prefix = new RegExp('^' + preprefix)
+
+    if (url[url.length-1]=='/') url = url.substr(0,url.length-1)
+    // function nlog (s) {
+    //     if (nodejs) {console.log(s)} else console.log('%c' + s, 'color: blue')
+    // }
+    
+
+    function send(args) {
+        if (args.method === 'get')
+            h2_get(args)
+
+        else if (args.method === 'set')
+            h2_set(args)
+
+        else
+            console.log('Need to implement', args.method.toUpperCase(), '!!!!!')
+    }
+
+    function connect () {
+        console.log("Hmm... I need to think about what connect() should do here...")
+        pipe.connected()
+    }
+
+    function parse_headers (str) {
+        // This string could contain a whole response.
+        // So first let's isolate to just the headers.
+        var end_of_headers = str.indexOf('\n\n'),
+            stuff_to_parse = str.substr(0, end_of_headers)
+
+        // Now let's grab everything from these headers
+        var headers = {},
+            regex = /([\w-]+): (.*)/g,
+            temp
+        while (temp = regex.exec(stuff_to_parse))
+            headers[temp[1].toLowerCase()] = temp[2]
+
+        return {headers, left_over: str.substr(end_of_headers + 2)}
+    }
+    function parse_version (str) { return JSON.parse(str) }
+    function parse_parents (str) {
+        console.log('parse_parents:',str);
+        var versions = str ? JSON.parse('[' + str + ']') : []
+        var result = {}
+        versions.forEach(v => result[v] = true)
+        return result
+    }
+    function h2_get ({key, subscribe}) {
+        key = rem_prefix(key)
+        function trySend() {
+            console.log('Fetching', url + '/' + key)
+            fetch(url + '/' + key, {method: 'GET', mode: 'cors',
+                                    headers: {'subscribe': ''}})
+                .then(function (res) {
+                    if (!res.ok) {
+                        console.error("Fetch failed!", Response)
+                        return
+                    }
+                    var reader = res.body.getReader()
+                    var decoder = new TextDecoder('utf-8')
+                    var buffer = ''
+                    function read() {
+                        reader.read().then(function (x) {
+                            var done = x.done, value = x.value
+                            if (!done) {
+                                buffer += decoder.decode(value)
+                                console.log('We have buffer', buffer)
+
+                                // Now try to parse it
+                                var {headers, left_over} = parse_headers(buffer)
+                                assert(headers['merge-type'] === 'sync9')
+
+                                if (headers['content-length']) {
+                                    console.log('Gonna grab the body, which is',
+                                                parseInt(headers['content-length']),
+                                                'characters of', left_over, ', or',
+                                                left_over.substr(0, parseInt(headers['content-length'])))
+                                    // Then this is setting the body.  First let's grab the body.
+                                    var body = left_over.substr(0, parseInt(headers['content-length']))
+                                    // Now remove it from the buffer
+                                    buffer = left_over.substr(body.length + 2)
+                                    console.log('Gonna send!', {method: 'set',
+                                               key: preprefix + key,
+                                               version: parse_version(headers['version']),
+                                               parents: parse_parents(headers['parents']),
+                                                                patches: ['= ' + body]})
+                                    pipe.recv({method: 'set',
+                                               key: preprefix + key,
+                                               version: parse_version(headers['version']),
+                                               parents: parse_parents(headers['parents']),
+                                               patches: ['= ' + body]})
+                                }
+
+                                else if (headers['patches']) {
+                                    // Then let's grab a sequence of patches!
+                                    console.log('This is patches!!!!!!!')
+                                }
+
+                                console.log('Good job! We did something!')
+
+                                // var m = buffer.match(/^(\d+)\n/)
+                                // while (m) {
+                                //     var content_length = parseInt(m[1])
+                                //     if (buffer.length >=
+                                //         content_length + m[1].length + 1) {
+                                //         var content = buffer.substr(m[1].length + 1,
+                                //                                     content_length + m[1].length + 1)
+                                //         buffer = buffer.substr(content_length + m[1].length + 4)
+                                //         console.log('Content is', content)
+                                //         console.log('And buffer is now', JSON.stringify(buffer))
+                                //         content = JSON.parse(content)
+                                            
+                                //         pipe.recv({method: 'set',
+                                //                    key: preprefix + key,
+                                //                    patches: content})
+                                //     }
+                                //     m = buffer.match(/^(\d+)\n/)
+
+                                read()
+                            }
+                        })
+                    }
+                    read()
+                })
+                .catch(function (err) {
+                    console.log("Fetch GET failed: ", err)
+                    setTimeout(trySend, 3000)
+                })
+        }
+        trySend()
+    }
+
+    function h2_set (obj, t) {
+        var h = {}
+        if (t.version) h.version = t.version
+        if (t.parents) h.parents = t.parents.map(JSON.stringify).join(', ')
+        var key = rem_prefix(obj.key)
+
+        var body = t.patch ? t.patch : JSON.stringify(obj)
+        function trySend(waitTime) {
+            fetch(url + "/" + key, {method: 'PUT', body: body,
+                                    headers: new Headers(h), mode: 'no-cors'})
+                .then(function (res) {
+                    res.text().then(function (text) {
+                        console.log('h2_set got a ', res.status, text)
+                    })
+                })
+                .catch(function (err) {
+                    console.log("Fetch SET failed: ", err);
+                    setTimeout(() => trySend(Math.min(waitTime * 5, 10000)), waitTime)
+                });
+        }
+        trySend(10);
+    }
+    
+    function h2_forget (key) {
+        var key = rem_prefix(key)
+        function trySend(waitTime) {
+            fetch(url + "/" + key, {method: 'FORGET', mode: 'cors'})
+                .then(function (res) {
+                    res.text().then(function (text) {
+                        console.log('h2_forget got a ', res.status, text)
+                    })
+                }).catch(function (err) {
+                    console.log("Fetch FORGET failed: ", err);
+                    setTimeout(() => trySend(Math.min(waitTime * 5, 10000)), waitTime)
+                });
+        }
+        trySend(10);
+    }
+    function h2_delete (key) {
+        var key = rem_prefix(key)
+        function trySend(waitTime) {
+            fetch(url + "/" + key, {method: 'DELETE', mode: 'cors'})
+                .then(function (res) {
+                    res.text().then(function (text) {
+                        console.log('h2_delete got a ', res.status, text)
+                    })
+                }).catch(function (err) {
+                    console.log("Fetch DELETE failed: ", err);
+                    setTimeout(() => trySend(Math.min(waitTime * 5, 10000)), waitTime)
+                });
+        }
+        trySend(10);
+    }
+
+    function add_prefix (key) {
+        return is_absolute.test(key) ? key : preprefix + key }
+    function rem_prefix (key) {
+        return has_prefix.test(key) ? key.substr(preprefix.length) : key }
+    function add_prefixes (obj) {
+        return bus.translate_keys(bus.clone(obj), add_prefix) }
+    function rem_prefixes (obj) {
+        return bus.translate_keys(bus.clone(obj), rem_prefix) }
+
+    // node(prefix).to_set    = function (obj, t) {
+    //     bus.set.fire(obj)
+    //     h2_set(obj, t)
+    // }
+    // bus(prefix).to_get    = function (key) {
+    //     h2_get(key),
+    //     keys_we_got.add(key)
+    // }
+    // bus(prefix).to_forget = function (key) {
+    //     h2_forget(key),
+    //     keys_we_got.delete(key)
+    // }
+    // bus(prefix).to_delete = h2_delete
+}
--- http-server.js
+++ http-server.js
@@ -1,1 +1,179 @@
-// Example braid-peer as a web server will go here
+// Example braid-peer as a web server
+
+module.exports = require['http-server'] = function add_http_server(node, certificate, private_key) {
+    var protocol = 'http',
+        port = '3007'
+
+    function http1_server () {
+        var free_the_cors = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*",
+            "Access-Control-Allow-Headers": "*"
+        }
+        var http = require('http')
+        var server = http.createServer()
+        server.listen(port, () => {
+            console.log('Listening on ' + protocol + '://<host>:' + port)
+        })
+        server.on('request', (req, res) => {
+            console.log("Got a request: {");
+            for (let [k, v] of Object.entries(req.headers))
+                console.log(`  ${k}: "${v}"`)
+            console.log("}")
+
+            switch (req.method) {
+            case 'GET':
+                console.log('Got a GET!!!')
+                res.writeHead(209, {'subscribe': true, ...free_the_cors})
+                res.write(`Version: "nooooodllle"
+Parents:
+Content-Type: application/json
+Merge-Type: sync9
+Content-Length: 81
+
+[{"text": "Hi, everyone!",
+  "author": {"type": "link", "value": "/user/tommy"}}]
+`)
+                // respond_sse(res.stream, req.url)
+                break
+            case 'OPTIONS':
+                console.log('## We got the options~!!')
+                res.writeHead(200, free_the_cors)
+                res.end()
+                // res.stream.end("forgotten")
+                break
+            case 'FORGET':
+            case 'PUT':
+            case 'PATCH':
+                console.log("Got a set: {");
+                for (let [k, v] of Object.entries(req.headers))
+                    console.log(`  ${k}: "${v}"`)
+                console.log("}")
+                res.stream.respond({':status': 200, ...free_the_cors})
+                res.stream.end("ack");
+                break
+            case 'DELETE':
+                break
+            default:
+                res.stream.respond({':status': 404})
+                res.stream.end()
+                break
+            }
+        })
+
+    }
+
+    http1_server()
+
+    function http2_server () {
+        var http = require('http2'),
+            fs = require('fs')
+        var free_the_cors = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*",
+            "Access-Control-Allow-Headers":
+            "Origin, X-Requested-With, Content-Type, Accept"
+        }
+
+        var server = http.createSecureServer({
+            // ca: (fs.existsSync(this.options.certs.certificate_bundle)
+            //      && require('split-ca')(this.options.certs.certificate_bundle)),
+            key:  private_key,
+            cert: certificate,
+            ciphers: "ECDHE-RSA-AES256-SHA384:DHE-RSA-AES256-SHA384"
+                + ":ECDHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA256"
+                + ":ECDHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA256"
+                + ":HIGH:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!SRP:!CAMELLIA",
+            honorCipherOrder: true
+        })
+
+        var server_2 = http.createServer()
+
+        server.on('error', (error) => {console.error(error)})
+
+        server.on('session', function (session) {
+            console.log("New Session!")
+            session.on('close', () => console.log("Session closed"))
+            session.on('goaway', () => console.log("Session goaway-ed"))
+            session.on('error', (e) => console.log("Session errored:", e))
+            session.on('timeout', () => console.log("Session timed out"))
+
+            var pipe = require('./pipe.js')({node, send, connect})
+
+            function send (args) {
+                console.log('SEND!!! <implement me..> :/', args)
+            }
+            function connect () { pipe.connected() }
+
+            var subscriptions_to_us = {}  // Every key that this session has gotton
+            console.log('h2_serve: New connection')
+            function h2_pubber (obj, t) {
+                log('h2_pubber:', obj, t)
+                var msg = {set: obj}
+                if (t.version) msg.version = t.version
+                if (t.parents) msg.parents = t.parents
+                if (t.patch)   msg.patch =   t.patch
+                if (t.patch)   msg.set    = msg.set.key
+                msg = JSON.stringify(msg)
+
+                if (global.network_delay) {
+                    console.log('>>>> DELAYING!!!', global.network_delay)
+                    obj = bus.clone(obj)
+                    setTimeout(() => {conn.write(msg)}, global.network_delay)
+                } else
+                    session.write(msg)
+
+                console.log('sockjs_s: SENT a', msg, 'to client')
+            }
+
+        })
+
+        server.on('request', function (req, res) {
+            console.log("Got a request: {");
+            for (let [k, v] of Object.entries(req.headers))
+                console.log(`  ${k}: "${v}"`)
+            console.log("}")
+
+            switch (req.method) {
+            case 'GET':
+                
+                respond_sse(res.stream, req.url);
+                break
+            case 'OPTIONS':
+                console.log('## We got the options~!!')
+                res.stream.respond({':status': 200, ...free_the_cors})
+                res.stream.end("forgotten")
+                break
+            case 'FORGET':
+                if (!res.stream.session.open_gets[req.url]) {
+                    res.stream.respond({':status': 404});
+                    res.stream.end();
+                } else {
+                    res.stream.session.open_gets[req.url].close()
+                    res.stream.respond({':status': 200, ...free_the_cors})
+                    res.stream.end("forgotten");
+                    delete res.stream.session.open_gets[req.url]
+                }
+                break
+            case 'PUT':
+            case 'PATCH':
+                console.log("Got a set: {");
+                for (let [k, v] of Object.entries(req.headers))
+                    console.log(`  ${k}: "${v}"`)
+                console.log("}")
+                res.stream.respond({':status': 200, ...free_the_cors})
+                res.stream.end("ack");
+                break
+            case 'DELETE':
+                res.stream.respond({':status': 200, ...free_the_cors});
+                res.stream.end("deeleeted de-1337-ed");
+                break
+            default:
+                res.stream.respond({':status': 404})
+                res.stream.end()
+                break
+            }
+        })
+        server.listen('3007', () => console.log(`Server listening!`));
+    }
+}
