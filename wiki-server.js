6ca87ea234c0288f361183c27d18c329546f41aa
My Name <glittle@gmail.com>
2020-04-25 18:17:46 -0700

    makes wiki-server serve bundle

--- wiki-server.js
+++ wiki-server.js
@@ -1,10 +1,29 @@
 
+var port = 3007
+
+require('./braid-bundler.js')
+var fs = require('fs')
+var bundle = fs.readFileSync('braid-bundle.js')
+var cb = (req, res) => {
+    res.writeHead(200)
+    res.end(bundle)
+}
+
+var server = (fs.existsSync('privkey.pem') && fs.existsSync('fullchain.pem')) ?
+    require('https').createServer({
+        key: fs.readFileSync('privkey.pem'),
+        cert: fs.readFileSync('fullchain.pem')
+    }, cb) :
+    require('http').createServer(cb)
+server.listen(port)
+var wss = new (require('ws').Server)({server})
+
 var node = require('./sqlite-store.js')(require('./node.js')(), 'db.sqlite')
 node.on_errors.push((key, origin) => node.unbind(key, origin))
 node.fissure_lifetime = 1000*60*60*24*7 // week
 node.compress()
 
-var wss = require('./networks/websocket-server.js')(node)
+require('./networks/websocket-server.js')(node, {wss})
 
 show_debug = true
 print_network = true
