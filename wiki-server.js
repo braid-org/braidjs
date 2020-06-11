8e1ec587c07eb63a3050f07c5f756913e199ffd0
Rafie Walker <slickytail.mc@gmail.com>
2020-06-11 00:40:10 -0700

    Better logging info on wiki server

--- wiki-server.js
+++ wiki-server.js
@@ -1,6 +1,32 @@
+// Better debugging log info
+['log','warn','error'].forEach((methodName) => {
+  const originalMethod = console[methodName];
+  console[methodName] = (...args) => {
+    try {
+      throw new Error();
+    } catch (error) {
+      originalMethod.apply(
+        console,
+        [
+          (
+            error
+            .stack // Grabs the stack trace
+            .split('\n')[2] // Grabs third line
+            .trim() // Removes spaces
+            .substring(3) // Removes three first characters ("at ")
+            .replace(__dirname, '') // Removes script folder path
+            .replace(/\s\(./, ' at ') // Removes first parentheses and replaces it with " at "
+            .replace(/\)/, '') // Removes last parentheses
+          ),
+          '\n',
+          ...args
+        ]
+      );
+    }
+  };
+});
 
-var port = 3007
-
+const port = 3009;
 require('./braid-bundler.js')
 var fs = require('fs')
 var bundle = fs.readFileSync('braid-bundle.js')
@@ -28,9 +54,14 @@ var ws = require('./networks/websocket-server.js')(node, {wss})
 
 console.log('keys at startup: ' + JSON.stringify(Object.keys(node.resources)))
 
-ws.on('connection', function(conn) {
+ws.on('connection', function(conn, req) {
+    const ip = req.socket.remoteAddress;
     conn.on('message', (msg) => {
-        if (!msg.match(/^\{"method"\:"(ping|pong)"\}$/))
-            console.log('GOT: ' + msg)
+        let data = JSON.parse(msg);
+        if (data.method != "ping" && data.method != "pong")
+            console.log(`Client at ${ip} sent:`);
+            console.group();
+            console.dir(data, {depth: 4});
+            console.groupEnd();
     })
 })
