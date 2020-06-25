5054c4867ef19d39c32a9ee98896fc0d209e3e24
My Name <glittle@gmail.com>
2020-06-24 18:45:48 -0700

    bloop

--- demos/wiki/wiki-server.js
+++ demos/wiki/wiki-server.js
@@ -3,7 +3,7 @@ const port = 3007;
 require('../../util/braid-bundler.js')
 var fs = require('fs')
 var bundle = fs.readFileSync('../../builds/braid-bundle.js')
-var wiki_client = fs.readFileSync('wiki-client.html')
+var wiki_client = fs.readFileSync('demos/wiki/wiki-client.html')
 var cb = (req, res) => {
     res.writeHead(200)
     res.end(req.url == '/braid-bundle.js' ? bundle : wiki_client)
@@ -19,11 +19,10 @@ server.listen(port)
 var wss = new (require('ws').Server)({server})
 
 var node = require('../../braid.js')()
-node.fissure_lifetime = 1000*60*60*24*7 // week
 require('../../util/sqlite-store.js')(node, 'db.sqlite')
 node.on_errors.push((key, origin) => node.unbind(key, origin))
 
-var ws = require('../../protocol-websocket//websocket-server.js')(node, {wss})
+var ws = require('../../protocol-websocket/websocket-server.js')(node, {wss})
 
 console.log('keys at startup: ' + JSON.stringify(Object.keys(node.resources)))
-
+console.log('serving on port: ' + port)
