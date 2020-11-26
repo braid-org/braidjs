313a4d483fad6c53c9168d47714507dae725ce29
Michael Toomim <toomim@gmail.com>
2020-11-25 20:16:58 -0800

    Fix up require() and wiki-perf after refactor

--- util/require.js
+++ util/require.js
@@ -1,7 +1,9 @@
 // These 8 lines let browsers import modules with require().
 function require (thing) {
     thing = thing.split('/')
-    thing = thing[thing.length-1].slice(0,-3)
+    thing = thing[thing.length-1]
+    if (thing.slice(-3) === '.js')
+        thing = thing.slice(0,-3)
     console.assert(require[thing], `require("${thing}") failed because <script src="${thing}"> is not working.`)
     return require[thing]
 }
