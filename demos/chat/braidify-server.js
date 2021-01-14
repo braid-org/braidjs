14ccb8f4a7dfeba8501442efdd185a56cf6fe4eb
Michael Toomim <toomim@gmail.com>
2021-01-14 00:43:57 -0800

    Add new chat demo

---
+++ demos/chat/braidify-server.js
@@ -0,0 +1,224 @@
+// Write an array of patches into the pseudoheader format.
+function generate_patches(res, patches) {
+    var patches_as_strings = false
+    // This will return something like:
+    // Patches: n
+    // 
+    // content-length: 14 // patch #1
+    // content-range: json=.range (or) json=[indices]
+    //
+    // ["json object"]
+    //
+    // content-length: x // patch #2
+    // ...
+    let out = `Patches: ${patches.length}\n`
+    for (let patch of patches) {
+        out += "\n"
+        if (patches_as_strings) {
+            // This should be rewritten to use sync9's patch parser.
+            var split = patch.match(/(.*?)\s*=\s*(.*)/)
+            assert(split.length == 3)
+            var range = split[1]
+            var change = split[2]
+        } else {
+            console.log('patch is', patch)
+            var range = patch.range,
+                change = patch.content
+            // if (res.getHeader('content-type') === 'application/json')
+            //     change = JSON.stringify(change)
+        }
+        console.log({range, change})
+        
+        out += `content-length: ${change.length}\n`
+        out += `content-range: json=${range}\n`
+        out += "\n"
+        out += `${change}\n`
+    }
+    return out
+}
+
+
+// This function reads num_patches in pseudoheader format from a
+// ReadableStream and then fires a callback when they're finished.
+function parse_patches (req, cb) {
+    var num_patches = req.headers.patches,
+        stream = req
+
+    let patches = []
+    let buffer = ""
+    if (num_patches === 0)
+        return cb(patches)
+
+    stream.on('data', function parse (chunk) {
+        while (patches.length < num_patches) {
+            // Merge the latest chunk into our buffer
+            buffer = (buffer + chunk)
+
+            // We might have an extra newline at the start.  (mike: why?)
+            buffer = buffer.trimStart()
+
+            // First parse the patch headers.  It ends with a double-newline.
+            // Let's see where that is.
+            var p_headers_length = buffer.indexOf("\n\n")
+
+            // Give up if we don't have a set of headers yet.
+            if (buffer.indexOf("\n\n") === -1)
+                return
+
+            // Now let's parse those headers.
+            var p_headers = require('parse-headers')(
+                buffer.substring(0, p_headers_length)
+            )
+
+            // Content-length tells us how long the body of the patch will be.
+            // TODO: Support Transfer-Encoding: Chunked in addition to content-length?
+            assert(p_headers['content-length'])
+            var body_length = parseInt(p_headers['content-length'])
+
+            // Give up if we don't have the full patch yet.
+            if (buffer.length < p_headers_length + 2 + body_length)
+                return
+
+            // Assume that content-range is of the form 'json=.index'
+            var patch_range = p_headers['content-range'].startsWith("json=") ?
+                p_headers['content-range'].substring(5) :
+                p_headers['content-range']
+            var patch_content =
+                buffer.substring(p_headers_length + 2,
+                                     p_headers_length + 2 + body_length)
+
+            // console.log('headers is', req.headers)
+            // if (req.headers['content-type'] === 'application/json')
+            //     patch_content = JSON.parse(patch_content)
+
+            // We've got our patch!
+            patches.push({range: patch_range, content: patch_content})
+
+            buffer = buffer.substring(p_headers_length + 2 + body_length)
+        }
+
+        // We got all the patches!  Pause the stream and tell the callback!
+        stream.pause()
+        cb(patches)
+    })
+    stream.on('end', () => {
+        // If the stream ends before we get everything, then return what we
+        // did receive
+        console.error('Stream ended!')
+        if (patches.length !== num_patches)
+            console.error(`Got an incomplete PUT: ${patches.length}/${num_patches} patches were received`)
+    })
+}
+
+function braidify (req, res, next) {
+    console.log('\n## Braidifying', req.method, req.url, req.headers.client)
+
+    // First, declare that we support Patches and JSON ranges.
+    res.setHeader('Range-Request-Allow-Methods', 'PATCH, PUT')
+    res.setHeader('Range-Request-Allow-Units', 'json')
+    res.setHeader("Patches", "OK")
+
+    // Extract braid info from headers
+    var version = req.headers.version && JSON.parse(req.headers.version),
+        parents = req.headers.parents && JSON.parse('['+req.headers.parents+']'),
+        client = req.headers['client'],
+        url = req.url.substr(1)
+
+    // Parse the subscribe header as one of these forms:
+    //
+    //   keep-alive
+    //   keep-alive=number
+    //
+    var subscribe = req.headers.subscribe
+    if (subscribe) {
+        let match = req.headers.subscribe.match(/keep-alive(=\w+)?/)
+        if (match)
+            subscribe =
+                match[1] ? {keep_alive: true} : {keep_alive: parseInt(match[1])}
+    }
+
+    // Define convenience variables
+    req.version   = version
+    req.parents   = parents
+    req.subscribe = subscribe
+
+    // Add the braidly request/response helper methods
+    res.sendVersion = (stuff) => send_version({res: res, ...stuff})
+    res.sendVersion = (stuff) => send_version({res: res, ...stuff})
+    req.patches = () => new Promise(
+        (done, err) => parse_patches(req, (patches) => done(patches))
+    )
+    req.patchesJSON = () => new Promise(
+        (done, err) => parse_patches(
+            req,
+            (patches) => done(patches.map(
+                p => ({...p, content: JSON.parse(p.content)})
+            ))
+        )
+    )
+    req.startSubscription = res.startSubscription =
+        function startSubscription (args) {
+            console.log('Starting subscription!!')
+            console.log('Timeouts are:',
+                        req.socket.server.timeout,
+                        req.socket.server.keepAliveTimeout)
+
+            // Let's disable the timeouts
+            req.socket.server.timeout = 0.0
+            req.setTimeout(0, x => console.log('Request timeout!', x))
+            res.setTimeout(0, x => console.log('Response timeout!', x))
+
+            // We have a subscription!
+            res.statusCode = 209
+            res.setHeader("subscribe", req.headers.subscribe)
+            res.setHeader('cache-control', 'no-cache, no-transform')
+            res.setHeader('content-type', 'application/json')
+
+            var connected = true
+            function disconnected () {
+                console.log(`Connection closed on ${req.url}`)
+
+                if (!connected) return
+                connected = false
+
+                // Now call the callback
+                if (args.onClose)
+                    args.onClose()
+            }
+
+            res.on('close',   disconnected)
+            res.on('finish',  disconnected)
+            req.on('abort',   disconnected)
+        }
+
+    next()
+}
+
+function send_version({res, version, parents, patches, body}) {
+    console.log('sending version', {version, parents, patches, body})
+    if (body) assert(typeof body === 'string');
+    (patches||[]).forEach(p=>assert(typeof p.content === 'string'))
+    if (version)
+        res.write(`Version: ${JSON.stringify(version)}\n`)
+    if (parents && parents.length) {
+        res.write(`Parents: ${parents.map(JSON.stringify).join(", ")}\n`)
+    }
+
+    res.write("Content-Type: application/json\n")
+    res.write("Merge-Type: sync9\n")
+    if (patches)
+        res.write(generate_patches(res, patches)) // adds its own newline
+    else if (body) {
+        // if (res.getHeader('content-type') === 'application/json')
+        //     body = JSON.stringify(body)
+        res.write('Content-Length: ' + body.length + '\n')
+        res.write('\n' + body + '\n')
+    } else {
+        console.trace("Missing body or patches")
+        process.exit()
+    }
+    res.write("\n")
+}
+
+
+module.exports = braidify
---
+++ demos/chat/server.js
@@ -0,0 +1,131 @@
+assert = require('assert')
+
+// Chat Data
+var state = {
+    '/chat': [
+        {text: 'Hello!'},
+        {text: 'This is a post!'},
+        {text: 'This is a post-modern!'}
+    ]
+}
+var chat_version = () => state['/chat'].length + ''
+var post_versions = {}
+curr_version = chat_version
+
+// Subscription data
+var subscriptions = {}
+var rhash = (req) => JSON.stringify([req.headers.client, req.url])
+
+
+// Create our HTTP bindings!
+var braidify = require('./braidify-server')
+var app = require('express')()
+
+// Middleware
+app.use(free_the_cors)
+app.use(braidify)
+
+// HTTP Routes
+function getter (req, res) {
+    // Make sure URL is valid
+    if (!(req.url in state)) {
+        res.statusCode = 404
+        res.end()
+        return
+    }
+
+    // Honor any subscription request
+    if (req.subscribe) {
+        res.startSubscription({ onClose: _=> delete subscriptions[rhash(req)] })
+        subscriptions[rhash(req)] = res
+    } else
+        res.statusCode = 200
+
+    // Send the current version
+    res.sendVersion({
+        version: curr_version(),
+        body: JSON.stringify(state[req.url])
+    })
+}
+app.get('/chat',     getter)
+
+app.put('/chat', async (req, res) => {
+    var patches = await req.patchesJSON()
+
+    assert(patches.length === 1)
+    assert(patches[0].range === '[-0:-0]')
+
+    state['/chat'].push(patches[0].content)
+
+    patches.forEach(patch => {
+        for (var k in subscriptions) {
+            var [client, url] = JSON.parse(k)
+            if (client !== req.headers.client && url === req.url)
+                subscriptions[k].sendVersion({
+                    version: curr_version(),
+                    patches: patches.map(
+                        p => ({...p, content: JSON.stringify(p.content)})
+                    )
+                })
+        }
+    })
+    res.statusCode = 200
+    res.end()
+})
+app.put('/post/:id', async (req, res) => {
+    var patches = await req.patchesJSON()
+
+    assert(patches.length === 1)
+    assert(patches[0].range === '')
+
+    state[req.url] = patches[0].content
+
+    for (var k in subscriptions) {
+        var [client, url] = JSON.parse(k)
+        if (client !== req.headers.client && url === req.url)
+            subscriptions[k].sendVersion({
+                version: curr_version(),
+                body: JSON.stringify(patches[0].content)
+            })
+    }
+
+    res.end()
+})
+
+// Now serve the HTML and client files
+sendfile = (f) => (req, res) => res.sendFile(f, {root:'.'})
+app.get('/',                   sendfile('index.html'));
+app.get('/braidify-client.js', sendfile('braidify-client.js'))
+app.use('/statebus', require('express').static('statebus'))
+
+// Free the CORS!
+function free_the_cors (req, res, next) {
+    console.log('free the cors!', req.method, req.url)
+    res.setHeader('Range-Request-Allow-Methods', 'PATCH, PUT')
+    res.setHeader('Range-Request-Allow-Units', 'json')
+    res.setHeader("Patches", "OK")
+    var free_the_cors = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS, HEAD, GET, PUT, UNSUBSCRIBE",
+        "Access-Control-Allow-Headers": "subscribe, client, version, parents, merge-type, content-type, patches, cache-control"
+    }
+    Object.entries(free_the_cors).forEach(x => res.setHeader(x[0], x[1]))
+    if (req.method === 'OPTIONS') {
+        res.writeHead(200)
+        res.end()
+    } else
+        next()
+}
+
+// Launch the https server
+var server = require('spdy').createServer(
+    {
+        cert:       require('fs').readFileSync('./certificate'),
+        key:        require('fs').readFileSync('./private-key'),
+        allowHTTP1: true
+    },
+    app
+)
+server.setTimeout(0, x => console.log('Server timeout!', x))
+console.log('Server timeouts:', server.timeout, server.keepAliveTimeout)
+server.listen(3009, _=> console.log('listening on port 3009...'))
