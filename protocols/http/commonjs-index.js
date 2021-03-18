ab6247330f286795be887da0c068ab579fc68d89
Michael Toomim <toomim@gmail.com>
2021-03-18 00:26:01 -0700

    More reorg cleanup

--- protocols/http/commonjs-index.js
+++
@@ -1,6 +0,0 @@
-var client = require('./http-client'),
-    server = require('./http-server')
-
-module.exports = { fetch: client.fetch,
-                   http: client.http,
-                   http_server: server }
