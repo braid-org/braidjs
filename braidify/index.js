27cae2f29f08ee30764a3eb97121d6f3d3f714f7
Michael Toomim <toomim@gmail.com>
2021-03-18 10:06:12 -0700

    Add comments to braidify index.*

--- braidify/index.js
+++ braidify/index.js
@@ -1,3 +1,7 @@
+// This is the root file for require('braidify').
+//
+// It combines the client and server files into one file.
+
 var client = require('./braidify-client'),
     server = require('./braidify-server'),
     http = (http) => client.http(server(http))
--- braidify/index.mjs
+++ braidify/index.mjs
@@ -1,8 +1,13 @@
+// This is the root file for es modules:
+//
+//    import {fetch, http} from 'braidify.js'
+//
+// This file combines the client and server files into one file.
+
 import braidify_client from './braidify-client.js' 
 import braidify_server from './braidify-server.js' 
-//var {fetch, http} = braidify_client
+
 var fetch = braidify_client.fetch
 var http = (http) => braidify_client.http(braidify_server(http))
 
 export { fetch, http }
-
