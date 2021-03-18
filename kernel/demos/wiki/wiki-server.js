f49c88791b2915591f4bdd2d15a033f090f4120c
Michael Toomim <toomim@gmail.com>
2021-03-18 01:01:46 -0700

    Fixed wiki demo

--- kernel/demos/wiki/wiki-server.js
+++ kernel/demos/wiki/wiki-server.js
@@ -1,9 +1,9 @@
 const port = 3007;
 
-require('../../util/braid-bundler.js')
+require('../../../util/braid-bundler.js')
 var fs = require('fs')
-var bundle = fs.readFileSync('builds/braid-bundle.js')
-var wiki_client = fs.readFileSync('demos/wiki/wiki-client.html')
+var bundle = fs.readFileSync('../../../builds/braid-bundle.js')
+var wiki_client = fs.readFileSync('wiki-client.html')
 var cb = (req, res) => {
     res.writeHead(200)
     res.end(req.url == '/braid-bundle.js' ? bundle : wiki_client)
@@ -18,14 +18,14 @@ var server = (fs.existsSync('certs/private-key') && fs.existsSync('certs/certifi
 server.listen(port)
 var wss = new (require('ws').Server)({server})
 
-var node = require('../../braid.js')()
-var store = require('../../util/sqlite-store.js')('db.sqlite')
-require('../../util/store.js')(node, store).then(node => {
+var node = require('../../node.js')()
+var store = require('../../sqlite-store.js')('db.sqlite')
+require('../../store.js')(node, store).then(node => {
     node.fissure_lifetime = 1000*60*60*8 // 8 hours
 
     node.on_errors.push((key, origin) => node.unbind(key, origin))
 
-    var ws = require('../../protocol-websocket/websocket-server.js')(node, {wss})
+    var ws = require('../../websocket-server.js')(node, {wss})
 
     console.log('keys at startup: ' + JSON.stringify(Object.keys(node.resources)))
     console.log('serving on port: ' + port)
