ff9229974db892bece493e3315cd7bd12844f77c
Michael Toomim <toomim@gmail.com>
2024-04-04 14:34:32 -0700

    Remove app.js cause it's in readme now

--- antimatter_wiki/app.js
+++
@@ -1,9 +0,0 @@
-// Example app
-
-var port = 60509,
-    domain = 'localhost:60509'
-
-process.on('uncaughtException', console.log)
-process.on('unhandledRejection', console.log)
-
-require('@braidjs/antimatter_wiki').serve({port, domain})
