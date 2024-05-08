b959963016db64701d99ebc52922ee4da399b753
The Whale <thewhale@Thes-MacBook-Pro.local>
2024-05-08 05:43:57 -0700

    braid-http:0.3.12 - bug fix

--- braid-http/braid-http-server.js
+++ braid-http/braid-http-server.js
@@ -195,8 +195,8 @@ function braidify (req, res, next) {
     res.setHeader('Range-Request-Allow-Units', 'json')
 
     // Extract braid info from headers
-    var version = req.headers.version && JSON.parse('['+req.headers.version+']'),
-        parents = req.headers.parents && JSON.parse('['+req.headers.parents+']'),
+    var version = ('version' in req.headers) && JSON.parse('['+req.headers.version+']'),
+        parents = ('parents' in req.headers) && JSON.parse('['+req.headers.parents+']'),
         peer = req.headers['peer'],
         url = req.url.substr(1)
 
