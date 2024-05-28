caac88df09e5e788c33aba75b1e19d2e74530298
dglittle <dglittle@users.noreply.github.com>
2024-05-28 14:16:56 -0700

    braid-http:0.3.20 - bug fix

--- braid-http/braid-http-client.js
+++ braid-http/braid-http-client.js
@@ -229,7 +229,7 @@ async function braid_fetch (url, params = {}) {
 
     // Now we define the subscription function we just used:
     function start_subscription (cb, error) {
-        if (!res.ok) {
+        if (!res.ok)
             throw new Error('Request returned not ok status:', res.status)
 
         if (res.bodyUsed)
